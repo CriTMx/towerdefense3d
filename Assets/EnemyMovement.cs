bb91cfba54fd7bc47007f2aea2c81010f4f17756
CriTMx <savefailedmc@gmail.com>
2025-01-06 13:29:17 +0530

    enemy ai fix

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    private float speed = 10f;
+    private float speed = 4f;
     private Transform target;
     private int waypointIndex = 0;
 
     
     void Start()
     {
-        target = WaypointsHandler.waypoints[0];
+        target = WaypointsHandler.waypoints[waypointIndex++];
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-        
+        Vector3 moveDir = (target.position - transform.position);
+        transform.Translate(moveDir.normalized * Time.deltaTime * speed);
+
+        if (waypointIndex < WaypointsHandler.waypoints.Length && 
+            (Vector3.Distance(transform.position, target.position) <= 0.2f))
+        {
+            target = WaypointsHandler.waypoints[waypointIndex++];
+        }
     }
 }
