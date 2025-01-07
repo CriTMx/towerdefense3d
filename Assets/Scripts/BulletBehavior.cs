edf09b062fa96cdaf1e6959e5ed7e6979f9aa8cd
CriTMx <savefailedmc@gmail.com>
2025-01-07 15:25:10 +0530

    explosion particles

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -6,13 +6,22 @@ public class BulletBehavior : MonoBehaviour
 {
     private Transform target;
     public float speed = 40f;
+    public float explosionRadius = 0f;
 
     public GameObject bulletImpactEffect;
+    public GameObject bulletExplosionEffect;
+    /*public GameObject bulletTrailEffect;*/
+
+
+    GameObject impactEffectInstance;
+    GameObject explosionEffectInstance;
+    /*GameObject trailEffectInstance;*/
 
     public void Seek(Transform _target)
     {
         target = _target;
     }
+
 
     void Update()
     {
@@ -36,10 +45,35 @@ public class BulletBehavior : MonoBehaviour
 
     void HitTarget()
     {
-        Destroy(gameObject);
-        GameObject effectInstance = (GameObject) Instantiate(bulletImpactEffect, transform.position, bulletImpactEffect.transform.rotation);
-        Destroy(effectInstance, 1.5f);
+        impactEffectInstance = (GameObject) Instantiate(bulletImpactEffect, transform.position, bulletImpactEffect.transform.rotation);
+        Destroy(impactEffectInstance, 1.5f);
 
-        Destroy(target);
+        if (explosionRadius > 0f)
+        {
+            DamageAOEAtTarget(target.gameObject);
+        } else
+        {
+            DamageSingleTarget(target.gameObject);
+        }
+
+        
+        Destroy(gameObject);
+    }
+
+    void DamageSingleTarget(GameObject _target)
+    {
+        if (_target.CompareTag("Enemy")) Destroy(_target);
+    }
+
+    void DamageAOEAtTarget(GameObject _target)
+    {
+        explosionEffectInstance = Instantiate(bulletExplosionEffect, _target.transform.position, bulletExplosionEffect.transform.rotation);
+        Destroy(explosionEffectInstance, 2f);
+
+        Collider[] affectedTargets = Physics.OverlapSphere(_target.transform.position, explosionRadius);
+        foreach (Collider targ in affectedTargets)
+        {
+            if (targ.CompareTag("Enemy")) Destroy(targ.gameObject);
+        }
     }
 }
